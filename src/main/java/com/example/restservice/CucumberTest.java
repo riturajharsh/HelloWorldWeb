3c16a0e40237f7707629e978aa4204fa027979b3
RITURAJ HARSHWARDHAN MISHRA <rm3313931@gmail.com>
2025-03-16 18:40:50 +0530

    Delete src/main/java/com/example/restservice/CucumberTest.java

--- src/main/java/com/example/restservice/CucumberTest.java
+++
@@ -1,8 +0,0 @@
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", glue = "com.example.restservice.steps")
-public class CucumberTest {
-}
